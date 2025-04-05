f6cf5a8dcc49f190156bbe285dee7156171d0e7a
andreykalinin <andreyypnz@yandex.ru>
2025-04-05 17:47:56 +0300

    all tasks resolved

--- homework/tasks/pg_task.py
+++ homework/tasks/pg_task.py
@@ -37,7 +37,16 @@ class ItemStorage:
         """
         # In production environment we will use migration tool
         # like https://github.com/pressly/goose
-        # YOUR CODE GOES HERE
+        query = """
+        CREATE TABLE IF NOT EXISTS items (
+            item_id INT PRIMARY KEY,
+            user_id INT NOT NULL,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL
+        );
+        """
+        async with self._pool.acquire() as connection:
+            await connection.execute(query)
 
     async def save_items(self, items: list[ItemEntry]) -> None:
         """
@@ -46,7 +55,15 @@ class ItemStorage:
         """
         # Don't use str-formatting, query args should be escaped to avoid
         # sql injections https://habr.com/ru/articles/148151/.
-        # YOUR CODE GOES HERE
+        query = """
+        INSERT INTO items (item_id, user_id, title, description)
+        VALUES ($1, $2, $3, $4);
+        """
+        values = [
+            (item.item_id, item.user_id, item.title, item.description) for item in items
+        ]
+        async with self._pool.acquire() as connection:
+            await connection.executemany(query, values)
 
     async def find_similar_items(
         self, user_id: int, title: str, description: str
@@ -54,4 +71,11 @@ class ItemStorage:
         """
         Напишите код для поиска записей, имеющих указанные user_id, title и description.
         """
-        # YOUR CODE GOES HERE
+        async with self._pool.acquire() as connection:
+            rows = await connection.fetch(
+                "SELECT * FROM items WHERE user_id = $1 AND title = $2 AND description = $3",
+                user_id,
+                title,
+                description,
+            )
+            return [ItemEntry(**row) for row in rows]
